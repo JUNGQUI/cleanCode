cdf81c63dc7eae398aa20f3d808f291fa73ef587
[백엔드 개발자]이정기 <ljk2518@gmail.com>
2020-11-12 23:50:43 +0900

    - 실습 2 init

--- src/test/java/com/jklee/cleancode/lambda/LambdaTest.java
+++ src/test/java/com/jklee/cleancode/lambda/LambdaTest.java
@@ -1,11 +1,38 @@
 package com.jklee.cleancode.lambda;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class LambdaTest {
+
+	// nextstep.fp.Lambda의 sumAll method
+	List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+//	int sumAllLambda(List<Integer> numbers) {
+//		return numbers.forEach();
+//	}
+
+	int sumAll(List<Integer> numbers) {
+		int total = 0;
+		for (int number : numbers) {
+			total += number;
+		}
+		return total;
+	}
+
+	\int sumAllEven(List<Integer> numbers) {
+		int total = 0;
+		for (int number : numbers) {
+			if (number % 2 == 0) {
+				total += number;
+			}
+		}
+		return total;
+	}
 
 	@Test
 	public void 이동() {
