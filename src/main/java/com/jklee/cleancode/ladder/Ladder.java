c152183ec44d3274fe38ca2ec90912a1ef195cba
jklee <ljk2518@gmail.com>
2020-11-17 16:04:56 +0900

    - ladder maker 완성 - 뭔가 애매하다.

--- src/main/java/com/jklee/cleancode/ladder/Ladder.java
+++ src/main/java/com/jklee/cleancode/ladder/Ladder.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 
@@ -28,6 +30,7 @@ public class Ladder {
 
 	List<String> ladderBuilder(List<String> name, int maxLength, int count) {
 		List<String> results = new ArrayList<>();
+
 		List<String> ladderBone = Arrays.stream(
 				"| ".repeat(name.size())
 						.split(" "))
@@ -36,25 +39,54 @@ public class Ladder {
 
 		for (int i = 0; i < count; i++) {
 			results.add(
-					ladderBuilderOneline(i == 0 ? name : ladderBone, maxLength, i == 0)
+					i == 0
+							? ladderBuilderOnelineName(name, maxLength)
+							: ladderBuilderOneline(ladderBone, maxLength)
 			);
 		}
 
 		return results;
 	}
 
-	String ladderBuilderOneline(List<String> inputs, int maxLength, boolean flag) {
-		String obj = flag ? " " : "-";
-		return String.valueOf(inputs.stream()
-				.map(
-						input -> {
-							int remainLength = maxLength - input.length();
-							return obj.repeat(remainLength) + input;
-						})
-				.collect(Collectors.toList()))
-				.replace("[", "")
-				.replace("]", "")
-				.replace(",", "");
+	String ladderBuilderOneline(List<String> inputs, int maxLength) {
+		AtomicBoolean flag = new AtomicBoolean(true);
+		Random random = new Random();
+
+		return reformatting(
+				String.valueOf(
+						inputs.stream()
+								.map(
+										input -> {
+											int remainLength = maxLength - input.length();
+
+											if (flag.get()) {
+												flag.set(false);
+												return " ".repeat(remainLength) + input;
+											}
+
+											if (random.nextInt(100) % 2 == 0) {
+												flag.set(true);
+												return "-".repeat(remainLength) + input;
+											}
+
+											return " ".repeat(remainLength) + input;
+										})
+								.collect(Collectors.toList())
+				)
+		);
+	}
+
+	String ladderBuilderOnelineName(List<String> inputs, int maxLength) {
+		return reformatting(
+				String.valueOf(inputs.stream()
+						.map(
+								input -> {
+									int remainLength = maxLength - input.length();
+									return " ".repeat(remainLength) + input;
+								})
+						.collect(Collectors.toList())
+				)
+		);
 	}
 
 	List<String> peopleSeparator(String people) {
@@ -63,7 +95,11 @@ public class Ladder {
 				.collect(Collectors.toList());
 	}
 
-	String makeBlankOrLine(boolean flag, int maxLength) {
-		return (flag ? " " : "-").repeat(Math.max(0, maxLength));
+	String reformatting(String needReform) {
+		return needReform
+				.replace("[", "")
+				.replace("]", "")
+				.replace(",", "");
 	}
+
 }
