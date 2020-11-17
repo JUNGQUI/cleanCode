25715e3ea39d6b2991b4d05f5675b443f4f9a00b
jklee <ljk2518@gmail.com>
2020-11-17 17:50:01 +0900

    - 사다리 실행

--- src/main/java/com/jklee/cleancode/ladder/Ladder.java
+++ src/main/java/com/jklee/cleancode/ladder/Ladder.java
@@ -7,28 +7,46 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Ladder {
 
-	public void makeLadder(String people, int count) {
-		List<String> peoples = peopleSeparator(people);
+	public List<String> makeLadder(List<String> peoples, List<String> results, int count) {
 
 		int maxLength = peoples.stream()
 				.map(String::length)
 				.max(Comparator.naturalOrder())
 				.orElseThrow();
 
-		List<String> results = ladderBuilder(peoples, maxLength, count);
-
-		for (String result : results) {
-			System.out.println(result);
-		}
+		return ladderBuilder(peoples, results, maxLength, count);
 	}
 
-	List<String> ladderBuilder(List<String> name, int maxLength, int count) {
+	public String playLadder(String condition, List<String> peoples, List<String> result, List<String> ladder) {
+		int idx = findIndex(condition, peoples);
+
+		if ("all".equals(condition)) {
+			return forwardLadderForAll(ladder, peoples, result);
+		}
+
+		return result.get(forwardLadder(ladder, idx));
+	}
+
+	int findIndex(String condition, List<String> peoples) {
+		int idx = -1;
+		int i = 0;
+
+		for (String people : peoples) {
+			idx = people.equals(condition) ? i : idx;
+			i++;
+		}
+
+		return idx;
+	}
+
+	List<String> ladderBuilder(List<String> name, List<String> result, int maxLength, int count) {
 		List<String> results = new ArrayList<>();
 
 		List<String> ladderBone = Arrays.stream(
@@ -41,11 +59,51 @@ public class Ladder {
 			results.add(
 					i == 0
 							? ladderBuilderOnelineName(name, maxLength)
-							: ladderBuilderOneline(ladderBone, maxLength)
+							: i == count - 1
+									? ladderBuilderOnelineName(result, maxLength)
+									: ladderBuilderOneline(ladderBone, maxLength)
 			);
 		}
 
 		return results;
+	}
+
+	String forwardLadderForAll(List<String> ladders, List<String> peoples, List<String> results) {
+		AtomicInteger i = new AtomicInteger();
+
+		return reformatting(
+				String.valueOf(
+						peoples.stream()
+								.map(people -> {
+									String result = people + " : " + results.get(forwardLadder(ladders, i.get())) + " ";
+									i.getAndIncrement();
+									return result;
+								})
+								.collect(Collectors.toList())
+				)
+		);
+	}
+
+	int forwardLadder(List<String> ladders, int index) {
+		for (String ladder: ladders) {
+			List<String> onelineLadder = Arrays.asList(ladder.split("\\|"));
+
+			if (onelineLadder.size() == 1) continue;
+
+			String leftLadder = index < 0 ? "" : onelineLadder.get(index);
+			String rightLadder = index+1 >= onelineLadder.size() ? "" : onelineLadder.get(index+1);
+
+			index = goLadderOneline(leftLadder, rightLadder, index);
+		}
+
+		return index;
+	}
+
+	int goLadderOneline(String leftLadder, String rightLadder, int startIdx) {
+		return leftLadder.contains("-")
+				? startIdx - 1
+				: rightLadder.contains("-")
+						? startIdx + 1 : startIdx;
 	}
 
 	String ladderBuilderOneline(List<String> inputs, int maxLength) {
--- src/test/java/com/jklee/cleancode/ladder/LadderTest.java
+++ src/test/java/com/jklee/cleancode/ladder/LadderTest.java
@@ -1,5 +1,8 @@
 package com.jklee.cleancode.ladder;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +12,53 @@ class LadderTest {
 
 	@Autowired private Ladder ladder;
 
+	private final List<String> peoples = new ArrayList<>();
+	private final List<String> results = new ArrayList<>();
+
+	@BeforeEach
+	void setup() {
+		peoples.add("poo");
+		peoples.add("JK");
+		peoples.add("Crong");
+		peoples.add("Meai");
+
+		results.add("1등");
+		results.add("2등");
+		results.add("3등");
+		results.add("4등");
+	}
+
 	@Test
 	void makeLadderTest() {
-		ladder.makeLadder("poo,JK,Crong,Meai", 5);
+		List<String> ladderResult = ladder.makeLadder(peoples, results, 5);
+
+		for (String ladders : ladderResult) {
+			System.out.println(ladders);
+		}
+	}
+
+	@Test
+	void makeAndPlayLadderTest() {
+		List<String> ladderResult = ladder.makeLadder(peoples, results, 5);
+
+		for (String ladders : ladderResult) {
+			System.out.println(ladders);
+		}
+
+		System.out.println(
+				ladder.playLadder(
+						"JK",
+						peoples,
+						results,
+						ladderResult)
+		);
+
+		System.out.println(
+				ladder.playLadder(
+						"all",
+						peoples,
+						results,
+						ladderResult)
+		);
 	}
 }
