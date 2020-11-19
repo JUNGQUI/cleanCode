1cfece3245e172dd5a991d5b695c53538d45d581
JUNGQUI <ljk2518@gmail.com>
2020-11-19 23:00:56 +0900

    질문 삭제하기
    - 수정

--- src/main/java/com/jklee/cleancode/qna/repository/UserRepository.java
+++ src/main/java/com/jklee/cleancode/qna/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.jklee.cleancode.qna.repository;
 
-import com.jklee.cleancode.optional.User;
+import com.jklee.cleancode.qna.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
