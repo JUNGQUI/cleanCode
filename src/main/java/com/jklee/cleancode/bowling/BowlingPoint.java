a99e76d3901fb4c9cfddbca188de71485ea4739d
JUNGQUI <ljk2518@gmail.com>
2020-11-22 16:06:20 +0900

    볼링 게임 draw table
    - 점수판 추가 완성
    - TODO
      - main drawTable refactoring
      - getter 제거

--- src/main/java/com/jklee/cleancode/bowling/BowlingPoint.java
+++ src/main/java/com/jklee/cleancode/bowling/BowlingPoint.java
@@ -4,26 +4,42 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
-@Builder
+@Builder(builderMethodName = "bowlingBuilder", toBuilder = true)
 @NoArgsConstructor
 @AllArgsConstructor
 public class BowlingPoint {
 	private int frame;
 	private BowlingStatus bowlingStatus;
-	private int finalPoint;
+	private int firstPoint;
+	private int secondPoint;
 
-	void updatePoint(int point) {
-		finalPoint += point;
+	public static BowlingPointBuilder builder (
+			int frame,
+			int firstPoint,
+			int secondPoint) {
+
+		BowlingStatus bowlingStatus = firstPoint == 10 ? BowlingStatus.STRIKE
+				: firstPoint + secondPoint == 10 ? BowlingStatus.SPARE : BowlingStatus.OPEN;
+
+		return BowlingPoint.bowlingBuilder()
+				.frame(frame)
+				.firstPoint(firstPoint)
+				.secondPoint(secondPoint)
+				.bowlingStatus(bowlingStatus);
 	}
 
-	public BowlingPoint bowlingPointBuilder (
-			int frame,
-			int point,
-			BowlingStatus bowlingStatus) {
-		return BowlingPoint.builder()
-				.frame(frame)
-				.bowlingStatus(bowlingStatus)
-				.finalPoint(point)
-				.build();
+	public String getPoint() {
+		return firstPoint == 10
+				? "X"
+				: firstPoint + secondPoint == 10
+						? firstPoint + "|" + "\\"
+						: (firstPoint == 0 ? "-" : firstPoint)
+								+ "|"
+								+ (secondPoint == 0 ? "-" : secondPoint);
+
+	}
+
+	public BowlingStatus getStatus() {
+		return bowlingStatus;
 	}
 }
