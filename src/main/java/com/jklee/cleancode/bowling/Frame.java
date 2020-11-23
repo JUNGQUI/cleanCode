137ec876a49b761d5649d31a243644fd75f06d38
JUNGQUI <ljk2518@gmail.com>
2020-11-23 21:29:13 +0900

    볼링 게임 점수 계산
    - init

---
+++ src/main/java/com/jklee/cleancode/bowling/Frame.java
@@ -0,0 +1,12 @@
+package com.jklee.cleancode.bowling;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Frame {
+
+}
